cb159f7011878472e49a54100f493b9dd1e091fa
Minsang Kim <ben399399@gmail.com>
2023-03-23 03:25:53 +0900

    feat: basic collision handling

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -1,14 +1,20 @@
 import { useGLTF } from '@react-three/drei'
+import { useTrimesh } from '@react-three/cannon'
 
-export const DiceRenderer = () => {
+export const DiceRenderer = (props) => {
   const { nodes, materials } = useGLTF('/dice.glb')
-  console.log(nodes, materials)
+  const [ref] = useTrimesh(() => ({
+    args: [nodes.Cube001.geometry.attributes.position.array, nodes.Cube001.geometry.index.array],
+    mass: 1,
+    position: [0, 5, 0],
+    ...props
+  }))
+  // console.log(nodes, materials)
+
   return (
-    // <Detailed ref={ref} distances={[0, 65, 80]}>
-    <>
+    <group ref={ref}>
       <mesh geometry={nodes.Cube001.geometry} material={materials.main_clr} />
       <mesh geometry={nodes.Cube001_1.geometry} material={materials.white} />
-    </>
-    // </Detailed>
+    </group>
   )
 }
--- src/Field.tsx
+++ src/Field.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
 import { DiceRenderer } from './Dice'
-import { Physics } from '@react-three/cannon'
+import { Physics, Debug } from '@react-three/cannon'
 import { Stats, OrbitControls } from '@react-three/drei'
+// import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
+import { DoubleSide } from 'three'
+import { usePlane } from '@react-three/cannon'
+
+const GreenSquare = (props) => {
+  const [ref] = usePlane(() => ({ mass: 0, ...props }))
+  return (
+    <mesh ref={ref} position={[0, 0, 0]} scale={[10, 10, 10]} receiveShadow>
+      <planeBufferGeometry />
+      <meshBasicMaterial color='green' side={DoubleSide} />
+    </mesh>
+  )
+}
 
 export const Field = () => {
   return (
     <div id='canvas-container' style={{ width: '500px', height: '500px' }}>
-      <Canvas shadows camera={{ position: [0, 2, 4] }}>
+      <Canvas shadows orthographic camera={{ position: [2, 2, 4] }}>
         <ambientLight intensity={0.1} />
         <directionalLight color='white' position={[0, 4, 8]} />
         <directionalLight color='white' position={[4, 4, -6]} />
         <directionalLight color='white' position={[-4, 4, -6]} />
-        <Physics gravity={[0, -9.8, 0]}>
+        <Physics>
+          {/* <Debug> */}
+          <GreenSquare rotation={[-Math.PI / 2, 0, 0]} receiveShadow />
           <DiceRenderer />
+          {/* </Debug> */}
         </Physics>
         <OrbitControls enableDamping={false} />
         <Stats />
+        {/* <EffectComposer multisampling={0}>
+          <DepthOfField target={[0, 0, 60]} focalLength={0.4} bokehScale={14} height={700} />
+        </EffectComposer> */}
       </Canvas>
     </div>
   )
