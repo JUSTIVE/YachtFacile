a5273c6ef6b682f3a1644546d2f6751f884d5099
Minsang Kim <ben399399@gmail.com>
2023-03-23 19:09:49 +0900

    feat: implemented rolling dice

--- src/Dice.tsx
+++ src/Dice.tsx
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { useBox } from '@react-three/cannon'
 
 export const DiceRenderer = (props) => {
+  const aclRef = useRef(null)
+  const posval = useRef(null)
   const { nodes, materials } = useGLTF('/dice.glb')
-  console.log(nodes, materials)
+  // console.log(nodes, materials)
 
-  const [ref] = useBox(() => ({
+  const [ref, api] = useBox(() => ({
     mass: 50,
     args: [2, 2, 2],
     ...props
   }))
 
+  const intensity = 10
+
+  const randomForce = () => {
+    return (Math.random() - 0.5) * intensity
+  }
+
+  useEffect(() => {
+    api.velocity.set(randomForce(), 0, randomForce())
+    api.angularVelocity.set(randomForce(), randomForce(), randomForce())
+
+    // api.torque.set(randomForce(), randomForce(), randomForce())
+  }, [api, aclRef.current])
+
+  useEffect(() => {
+    aclRef.current = new Accelerometer({ frequency: 60 })
+    aclRef.current.onerror = (event) => {
+      // Handle runtime errors.
+      if (event.error.name === 'NotAllowedError') {
+        console.log('Permission to access sensor was denied.')
+      } else if (event.error.name === 'NotReadableError') {
+        console.log('Cannot connect to the sensor.')
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!aclRef.current) return
+
+    aclRef.current.onreading = (e) => {
+      console.log(e), (posval.current = e)
+    }
+    aclRef.current.start()
+  }, [aclRef.current])
+
   return (
-    <group ref={ref} {...props}>
-      <mesh castShadow receiveShadow geometry={nodes.Object_2.geometry} material={materials['']} scale={0.1} />
-    </group>
+    <>
+      <group ref={ref} {...props}>
+        <mesh castShadow receiveShadow geometry={nodes.Object_2.geometry} material={materials['']} scale={0.1} />
+      </group>
+    </>
   )
 }
--- src/Field.tsx
+++ src/Field.tsx
@@ -4,31 +4,87 @@ import { DiceRenderer } from './Dice'
 import { Physics, Debug } from '@react-three/cannon'
 import { Stats, OrbitControls } from '@react-three/drei'
 import { EffectComposer, DepthOfField } from '@react-three/postprocessing'
-import { DoubleSide } from 'three'
-import { usePlane } from '@react-three/cannon'
+import { useBox } from '@react-three/cannon'
 
-const GreenSquare = (props) => {
-  const [ref] = usePlane(() => ({ mass: 0, ...props }))
+const DiceBox = (props) => {
+  const boxBottomSize = 15
+
+  const [backLeftRef] = useBox(() => ({
+    mass: 0,
+    args: [boxBottomSize, boxBottomSize / 2, 0.01],
+    position: [-boxBottomSize / 2, boxBottomSize / 4, 0],
+    rotation: [0, Math.PI / 2, 0],
+    ...props
+  }))
+
+  const [backRightRef] = useBox(() => ({
+    mass: 0,
+    args: [boxBottomSize, boxBottomSize / 2, 0.01],
+    position: [0, boxBottomSize / 4, -boxBottomSize / 2],
+    rotation: [0, 0, 0],
+    ...props
+  }))
+
+  const [frontLeftRef] = useBox(() => ({
+    mass: 0,
+    args: [boxBottomSize, boxBottomSize / 2, 0.01],
+    position: [0, boxBottomSize / 4, boxBottomSize / 2],
+    rotation: [0, Math.PI, 0],
+    ...props
+  }))
+
+  const [frontRightRef] = useBox(() => ({
+    mass: 0,
+    args: [boxBottomSize, boxBottomSize / 2, 0.01],
+    position: [boxBottomSize / 2, boxBottomSize / 4, 0],
+    rotation: [0, -Math.PI / 2, 0],
+    ...props
+  }))
+
+  const [bottomRef] = useBox(() => ({
+    mass: 0,
+    args: [boxBottomSize, boxBottomSize, 0.01],
+    rotation: [-Math.PI / 2, 0, 0],
+    ...props
+  }))
   return (
-    <mesh ref={ref} position={[0, 0, 0]} scale={[15, 15, 0]} receiveShadow>
-      <planeBufferGeometry args={[25, 25]} />
-      <meshBasicMaterial side={DoubleSide} color={'white'} />
-    </mesh>
+    <>
+      <mesh ref={backLeftRef} receiveShadow>
+        <planeGeometry args={[boxBottomSize, boxBottomSize / 2]} />
+        <meshStandardMaterial attach={'material'} color={'#fff'} />
+      </mesh>
+      <mesh ref={backRightRef} receiveShadow>
+        <planeGeometry args={[boxBottomSize, boxBottomSize / 2]} />
+        <meshStandardMaterial attach={'material'} color={'#fff'} />
+      </mesh>
+      <mesh ref={frontLeftRef} receiveShadow>
+        <planeGeometry args={[boxBottomSize, boxBottomSize / 2]} />
+        <meshStandardMaterial attach={'material'} color={'#fff'} />
+      </mesh>
+      <mesh ref={frontRightRef} receiveShadow>
+        <planeGeometry args={[boxBottomSize, boxBottomSize / 2]} />
+        <meshStandardMaterial attach={'material'} color={'#fff'} />
+      </mesh>
+      <mesh ref={bottomRef} receiveShadow>
+        <planeGeometry args={[boxBottomSize, boxBottomSize]} />
+        <meshStandardMaterial attach={'material'} color={'#fff'} />
+      </mesh>
+    </>
   )
 }
 
 export const Field = () => {
   return (
     <div id='canvas-container' style={{ width: '100vw', height: '500px' }}>
-      <Canvas shadows={'soft'} orthographic camera={{ zoom: 30, near: -100, far: 100, position: [2, 2, 2] }}>
+      <Canvas shadows={'soft'} orthographic camera={{ zoom: 20, near: -100, far: 100, position: [2, 2, 2] }}>
         <color attach='background' args={['#222']} />
         <ambientLight intensity={0.1} />
         <directionalLight color='white' position={[0, 4, 8]} castShadow />
-        {/* <directionalLight color='white' position={[4, 4, -6]} castShadow />
-        <directionalLight color='white' position={[-4, 4, -6]} castShadow /> */}
+        <directionalLight color='white' position={[4, 4, -6]} castShadow />
+        <directionalLight color='white' position={[-4, 4, -6]} castShadow />
         <Physics>
           {/* <Debug> */}
-          <GreenSquare rotation={[-Math.PI / 2, 0, 0]} />
+          <DiceBox />
           {Array.from({ length: 5 }).map((_, i) => (
             <DiceRenderer position={[0, i * 3 + 2, 0]} key={`${i}-dice`} />
           ))}
